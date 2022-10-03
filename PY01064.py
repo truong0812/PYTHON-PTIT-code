1aa6ccb76539ed718685fc0b77302c8c3b18fb58
Hoàng Trường <87565382+truong0812@users.noreply.github.com>
2022-10-03 22:01:19 +0700

    Add files via upload

---
+++ PY01064.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    t = int(input())
+    while t > 0:
+        n, k = map(int, input().split())
+        if k % 2 == 1:
+            print('A')
+        else:
+            cnt = 0
+            while k % 2 == 0:
+                k /= 2
+                cnt += 1
+            print(chr(65 + cnt))
+        t -= 1
+        
---
+++ PY02061.py
@@ -0,0 +1,19 @@
+for i in range(int(input())):
+        n, m = map(int, input().split())
+        h = [[0 for _ in range(m)] for _ in range(n)]
+        k = [[0 for _ in range(3)] for _ in range(3)]
+        for i in range(n):
+            h[i] = list(map(int, input().split()))
+        for i in range(3):
+            k[i] = list(map(int, input().split()))
+        res = [[0 for _ in range(n - 2)] for _ in range(m - 2)]
+        ans = 0
+        for i in range(n - 2):
+            for j in range(m - 2):
+                for p in range(3):
+                    for q in range(3):
+                        res[i][j] += h[i + p][j + q] * k[p][q]
+                ans += res[i][j]
+        print(ans)
+    
+
