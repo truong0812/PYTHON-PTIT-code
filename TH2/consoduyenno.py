1690b5d74739159f0efc43c780418fabfa142bca
Hoàng Trường <87565382+truong0812@users.noreply.github.com>
2022-10-23 22:32:39 +0700

    Add files via upload

---
+++ TH2/consoduyenno.py
@@ -0,0 +1,6 @@
+for t in range(int(input())):
+    s = input()
+    if(s[0] == s[-1]):
+        print('YES')
+    else:
+        print('NO')
---
+++ TH2/xulyvanban.py
@@ -0,0 +1,11 @@
+from sys import stdin
+if __name__ == '__main__':
+    for line in stdin:
+        sen = ''
+        for x in line.split():
+            if x[-1] == '?' or x[-1] == '!' or x[-1] == '.':
+                sen += x[:-1]
+                print(sen.strip().capitalize())
+                sen = ''
+            else:
+                sen += x + ' '
